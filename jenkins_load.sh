dd7ed9df971eb961758058325f9dad775c49f9c9
lesekr <lesekr@hotmail.com>
2023-10-01 06:25:07 +0000

    jenkinsload_changes

---
+++ jenkins_load.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo\https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo yum upgrade -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
