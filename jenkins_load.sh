d1e9a23cb48af5d6f2c640d6ac56632429b1fca2
lesekr <lesekr@hotmail.com>
2023-10-02 20:21:48 +0000

    changes

--- jenkins_load.sh
+++ jenkins_load.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo\https://pkg.jenkins.io/redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-sudo yum upgrade -y
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-11-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo systemctl status jenkins
