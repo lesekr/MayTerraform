0a5774566cc6088d85773f32a406f6ef67351acb
lesekr <lesekr@hotmail.com>
2023-10-02 20:21:25 +0000

    changes

--- providers.tf
+++ providers.tf
@@ -13,17 +13,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
+#Build a VPC
 resource "aws_vpc" "example" {
   cidr_block = "172.31.0.0/16"
-}
-
-#create s3 bucket
-resource "aws_s3_bucket" "ekrissinbucketmay" {
-  bucket = var.bucketname
-
-  tags = {
-    Name        = "ekrissinbucketmay23"
-    Environment = "project_20"
-  }
 }
